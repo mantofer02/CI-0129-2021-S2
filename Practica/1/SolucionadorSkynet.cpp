b2b591c39521598e12308a2f6e6ee3b7321153f2
Marco Ferraro <mantofer2000@gmail.com>
2021-09-08 14:53:34 -0600

    Working

--- Practica/1/SolucionadorSkynet.cpp
+++ Practica/1/SolucionadorSkynet.cpp
@@ -6,32 +6,43 @@
  
 Solucion * SolucionadorSkynet::solucione( Problema * problema){
     Estado  * inicio = problema->getEstadoInicial();
-    Lista * pasos = problema->getSiguientes(inicio);
-    Lista * siguienteGen;
+  
+    
+    Lista * solucion = new Lista();
+    Lista * explorados = new Lista();
+    Lista * frontera = problema->getSiguientes(inicio);
+    Estado * estadoActual = frontera->pop_front();
+    
+    solucion->push_front(inicio);
+    explorados->push_back(inicio);
+
+    
+
     int haySolucion = 0;
 
-    Lista::Iterador it = pasos->begin();
+    Lista::Iterador it;
     
     while (!haySolucion) {
-      if (problema->esSolucion(*it)) {
-        haySolucion = 1;
-        // para meter la ultima, corregir
-        siguienteGen = problema->getSiguientes(*it);
-        for (Lista::Iterador sigIt = siguienteGen->begin(); sigIt != siguienteGen->end(); sigIt++)
-        {
-          pasos->push_back(*sigIt);
+      if (explorados->buscar(estadoActual) == explorados->end()) {
+        std::cout << "NOT FAIL" << std::endl;
+        if (problema->esSolucion(estadoActual)) {
+          haySolucion = 1;
+        } else {
+          siguienteGen = problema->getSiguientes(estadoActual);
+          for (it = siguienteGen->begin(); it != siguienteGen->end(); it++) {
+            frontera->push_back(*it);
+          }
+          explorados->push_back(estadoActual);
+          solucion->push_back(estadoActual);
+          estadoActual = frontera->pop_back();
         }
       } else {
-        siguienteGen = problema->getSiguientes(*it);
-        for (Lista::Iterador sigIt = siguienteGen->begin(); sigIt != siguienteGen->end(); sigIt++)
-        {
-          pasos->push_back(*sigIt);
-        }
-        it++;
+        solucion->pop_back();
+        estadoActual = frontera->pop_back();
       }
     }
     
-    Solucion * solucionMala = new Solucion( pasos );
+    Solucion * solucionMala = new Solucion(solucion);
     delete inicio;
     return solucionMala;
 }
