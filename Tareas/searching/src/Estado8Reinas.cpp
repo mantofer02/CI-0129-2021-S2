a68a4e2c03683a43085f257d45c5f20e94c1841d
Gabriel Revillat <gabrielrevillat@yahoo.es>
2021-09-20 23:15:25 -0600

    Cambio en el método cargar de Estado8Reinas

--- Tareas/searching/src/Estado8Reinas.cpp
+++ Tareas/searching/src/Estado8Reinas.cpp
@@ -1,74 +1,98 @@
-// #include "../headers/Estado8Reinas.h"
-// #include "../headers/Estado.h"
+#include "../headers/Estado8Reinas.h"
+#include "../headers/Estado.h"
 
-// Estado8Reinas::Estado8Reinas()
-// {
-//   srand(time(NULL));
+Estado8Reinas::Estado8Reinas()
+{
+  srand(time(NULL));
 
-//   for (int fila = 0; fila < 8; ++fila)
-//     for (int columna = 0; columna < 8; ++columna)
-//       this->tablero[fila][columna] = 0;
+  for (int fila = 0; fila < 8; ++fila)
+    for (int columna = 0; columna < 8; ++columna)
+      this->tablero[fila][columna] = 0;
 
-//   for (int fila = 0; fila < 8; ++fila)
-//   {
-//     this->tablero[fila][rand() % 7] = 1;
-//   }
-// }
+  for (int fila = 0; fila < 8; ++fila)
+  {
+    this->tablero[fila][rand() % 7] = 1;
+  }
+}
 
-// Estado8Reinas* Estado8Reinas::clonar()
-// {
-//   Estado8Reinas* nuevoEstado = new Estado8Reinas();
-//   for (int fila = 0; fila < 8; ++fila)
-//     for (int columna = 0; columna < 8; ++columna)
-//       nuevoEstado->tablero[fila][columna] = this->tablero[fila][columna];
+Estado8Reinas* Estado8Reinas::clonar()
+{
+  Estado8Reinas* nuevoEstado = new Estado8Reinas();
+  for (int fila = 0; fila < 8; ++fila)
+    for (int columna = 0; columna < 8; ++columna)
+      nuevoEstado->tablero[fila][columna] = this->tablero[fila][columna];
 
-//   return nuevoEstado;
-// }
+  return nuevoEstado;
+}
 
-// std::istream& Estado8Reinas::cargar(std::istream& entrada)
-// {
-//   std::string textoEntrada;
+std::istream& Estado8Reinas::cargar(std::istream& entrada)
+{
+  std::string textoEntrada;
+  int contadorDeReinas = 0;
+  int filaActual = 0;
+  int columnaActual = 0;
+  bool stop = false;
 
-//   for (int fila = 0; fila < 8; ++fila)
-//     for (int columna = 0; columna < 8; ++columna)
-//     {
-//       std::getline(entrada, textoEntrada);
+  for (int fila = 0; fila < 8; ++fila)
+  {
+    for (int columna = 0; columna < 8 && !stop; ++columna)
+    {
+      std::getline(entrada, textoEntrada);
 
-//       if (textoEntrada.size() == 1 && (textoEntrada == "0" || textoEntrada == "1"))
-//         this->tablero[fila][columna] = atoi(textoEntrada.c_str());
-//       else
-//         throw "Su entrada debe ser 0 (espacio vac�o) o 1 (reina).";
-//     }
-      
-// }
+      if (textoEntrada.size() == 1 && (textoEntrada == "0" || textoEntrada == "1"))
+      {
 
-// std::ostream& Estado8Reinas::imprimir(std::ostream& salida)
-// {
-//   for (int fila = 0; fila < 8; ++fila)
-//   {
-//     for (int columna = 0; columna < 8; ++columna)
-//       salida << '[' << (this->tablero[fila][columna] ? 'Q' : '_') << ']';
+        this->tablero[fila][columna] = atoi(textoEntrada.c_str());
+        ++contadorDeReinas;
 
-//     salida << '\n';
-//   }
+        if (contadorDeReinas == 8)
+        {
+          filaActual = fila;
+          columnaActual = columna;
+          stop = true;
+        }
+      }
+      else
+      {
+        std::cerr << "Su entrada debe ser 0 (espacio vacío) o 1 (reina).\n";
+        stop = true;
+      }
+    }
+  }
 
-//   return salida;
-// }
+  for (; filaActual < 8; ++filaActual)
+    for (; columnaActual < 8; ++columnaActual)
+      this->tablero[filaActual][columnaActual] = 0;
 
-// int Estado8Reinas::operator==(Estado* otroEstado)
-// {
-//   Estado8Reinas* copiaEstado = dynamic_cast<Estado8Reinas*>(otroEstado);
+  return entrada;
+}
 
-//   for (int fila = 0; fila < 8; ++fila)
-//     for (int columna = 0; columna < 8; ++columna)
-//       if (this->tablero[fila][columna] != copiaEstado->tablero[fila][columna])
-//         return false;
+std::ostream& Estado8Reinas::imprimir(std::ostream& salida)
+{
+  for (int fila = 0; fila < 8; ++fila)
+  {
+    for (int columna = 0; columna < 8; ++columna)
+      salida << '[' << (this->tablero[fila][columna] ? 'Q' : '_') << ']';
 
-//   return true;
-// }
+    salida << '\n';
+  }
 
-// int Estado8Reinas::operator!=(Estado* otroEstado)
-// {
-//   Estado8Reinas* copiaEstado = dynamic_cast<Estado8Reinas*>(otroEstado);
-//   return !(*this == copiaEstado);
-// }
+  return salida;
+}
+
+int Estado8Reinas::operator==(Estado* otroEstado)
+{
+  Estado8Reinas* copiaEstado = dynamic_cast<Estado8Reinas*>(otroEstado);
+
+  for (int fila = 0; fila < 8; ++fila)
+    for (int columna = 0; columna < 8; ++columna)
+      if (this->tablero[fila][columna] != copiaEstado->tablero[fila][columna])
+        return false;
+
+  return true;
+}
+
+int Estado8Reinas::operator!=(Estado* otroEstado)
+{
+  return !(*this == otroEstado);
+}
