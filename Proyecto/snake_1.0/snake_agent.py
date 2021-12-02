a64f2ac41935fd1d1646dfe11f38bbd93fe527c8
Marco Ferraro <mantofer2000@gmail.com>
2021-12-02 17:12:36 -0600

    Changes

--- Proyecto/snake_1.0/snake_agent.py
+++ Proyecto/snake_1.0/snake_agent.py
@@ -2,12 +2,16 @@ import pygame
 import random
 from collections import namedtuple
 from pygame import font
+import numpy as np
+import os
+import csv
 
 from pygame.version import PygameVersion
 import direction as dir
 
 BLOCK_SIZE = 20
 SPEED = 7
+SPEED_INCREASE = 1
 
 # Colors
 BLACK = (0, 0, 0)
@@ -83,7 +87,7 @@ class SnakeAgent():
 
     if self.head == self.food:
       self.score += 1
-      # self.speed += 1
+      self.speed += SPEED_INCREASE
       self.place_food()
     else:
       self.snake.pop()
@@ -133,14 +137,100 @@ class SnakeAgent():
     
     return False
 
+  def is_collision(self, pt=None):
+    if pt is None:
+      pt = self.head
+    if pt.x > self.widht - BLOCK_SIZE or pt.x < 0 or pt.y > self.height - BLOCK_SIZE or pt.y < 0:
+      return True
+    
+    if self.head in self.snake[1:]:
+      return True
+    
+    return False
+
+  def get_state(self):
+    head = self.snake[0]
+    point_l = Point(head.x - 20, head.y)
+    point_r = Point(head.x + 20, head.y)
+    point_u = Point(head.x, head.y - 20)
+    point_d = Point(head.x, head.y + 20)
+
+    dir_l = self.direction == dir.Direction.LEFT
+    dir_r = self.direction == dir.Direction.RIGHT
+    dir_u = self.direction == dir.Direction.UP
+    dir_d = self.direction == dir.Direction.DOWN
+
+    state = [
+      # Danger straight
+      (dir_r and self.is_collision(point_r)) or
+      (dir_l and self.is_collision(point_l)) or
+      (dir_u and self.is_collision(point_u)) or
+      (dir_d and self.is_collision(point_d)),
+
+      # Danger right
+      (dir_r and self.is_collision(point_d)) or
+      (dir_l and self.is_collision(point_u)) or
+      (dir_u and self.is_collision(point_r)) or
+      (dir_d and self.is_collision(point_l)),
+
+      # Danger left
+      (dir_r and self.is_collision(point_u)) or
+      (dir_l and self.is_collision(point_d)) or
+      (dir_u and self.is_collision(point_l)) or
+      (dir_d and self.is_collision(point_r)),
+
+      # Move direction
+      dir_l,
+      dir_r,
+      dir_u,
+      dir_d,
+
+      self.food.x < self.head.x, # food left
+      self.food.x > self.head.x, # food right
+      self.food.y < self.head.y, # food up
+      self.food.y > self.head.y, # food down
+
+      self.speed,
+      self.score
+    ]
+
+    return np.array(state, dtype=int)
+
+def save_data(data, file_name):
+  data_folder_path = './data'
+  column_labels = [
+    'danger_straight', 'danger_right', 'danger_left', 'dir_left', 'dir_right', 'dir_up', 'dir_down', 
+    'food_left', 'food_right', 'food_up', 'food_down',
+    'speed', 'score'
+    ]
+
+  if not os.path.exists(data_folder_path):
+    os.makedirs(data_folder_path)
+
+  with open(data_folder_path + str('/') + file_name + str('.csv'), 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(column_labels)
+    for i in data:
+      writer.writerow(i)
+
+
+
+
 if __name__ == '__main__':
+  print('Enter user name')
+  file_name = input()
+  data = []
+
   agent = SnakeAgent()
   game_over = False
 
   while game_over == False:
     game_over, score = agent.play_step()
+    data.append(agent.get_state())
 
     # break if loose
   print('Final Score', score)
+  save_data(data, file_name)
+  print(file_name)
 
   pygame.quit()
